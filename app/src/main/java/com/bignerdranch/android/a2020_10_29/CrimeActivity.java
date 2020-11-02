78ffdc5531b9e0fc493ca514953d58e095c0fbe5
Jongwoo Lim <limjongwoo1994@gmail.com>
2020-11-01 23:03:15 -0700

    second commit

--- app/src/main/java/com/bignerdranch/android/a2020_10_29/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/a2020_10_29/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.a2020_10_29;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -9,6 +11,16 @@ public class CrimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
